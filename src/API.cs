cfb1a44b0fa6051b8749a0b2ff9ec752257898b4
Hi_old <hi_old@aliyun.com>
2021-08-07 15:05:55 +0800

    兼容无缝重启

--- src/API.cs
+++ src/API.cs
@@ -58,7 +58,7 @@ namespace HioldMod
                 }
             }
 
-            
+
 
 
             //判断版本情况
@@ -115,10 +115,20 @@ namespace HioldMod
 
 
             //赋值主文件进行加载
-            File.Delete(AssemblyPath + "main.dlc");
-            File.Copy(AssemblyPath + "main.bin", AssemblyPath + "main.dlc");
-            //反射获取数据
-            LoadAssembly(AssemblyPath + "main.dlc");
+            try
+            {
+                File.Delete(AssemblyPath + "main.dlc");
+                File.Copy(AssemblyPath + "main.bin", AssemblyPath + "main.dlc");
+                //反射获取数据
+                LoadAssembly(AssemblyPath + "main.dlc");
+            }
+            catch (Exception)
+            {
+                File.Delete(AssemblyPath + "main.dlc2");
+                File.Copy(AssemblyPath + "main.bin", AssemblyPath + "main.dlc2");
+                //反射获取数据
+                LoadAssembly(AssemblyPath + "main.dlc2");
+            }
         }
 
         //加载mod
@@ -174,6 +184,33 @@ namespace HioldMod
             {
                 return false;
             }
+        }
+
+        ///<summary>
+        ///生成随机字符串 
+        ///</summary>
+        ///<param name="length">目标字符串的长度</param>
+        ///<param name="useNum">是否包含数字，1=包含，默认为包含</param>
+        ///<param name="useLow">是否包含小写字母，1=包含，默认为包含</param>
+        ///<param name="useUpp">是否包含大写字母，1=包含，默认为包含</param>
+        ///<param name="useSpe">是否包含特殊字符，1=包含，默认为不包含</param>
+        ///<param name="custom">要包含的自定义字符，直接输入要包含的字符列表</param>
+        ///<returns>指定长度的随机字符串</returns>
+        public static string GetRandomString(int length, bool useNum, bool useLow, bool useUpp, bool useSpe, string custom)
+        {
+            byte[] b = new byte[4];
+            new System.Security.Cryptography.RNGCryptoServiceProvider().GetBytes(b);
+            Random r = new Random(BitConverter.ToInt32(b, 0));
+            string s = null, str = custom;
+            if (useNum == true) { str += "0123456789"; }
+            if (useLow == true) { str += "abcdefghijklmnopqrstuvwxyz"; }
+            if (useUpp == true) { str += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; }
+            if (useSpe == true) { str += "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"; }
+            for (int i = 0; i < length; i++)
+            {
+                s += str.Substring(r.Next(0, str.Length - 1), 1);
+            }
+            return s;
         }
 
     }
